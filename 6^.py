38e42e5f5569d32936cd5fe04859fdd41cfe4730
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-07-09 16:03:33 +0530

    Add files via upload

---
+++ 6^.py
@@ -0,0 +1,10 @@
+a,b=map(str,input().split())
+if(len(a)!=len(b)):
+    print("no")
+else:
+    d1=[a.count(i) for i in a]
+    d2=[b.count(i) for i in b]
+if(d1==d2):
+    print("yes")
+else:
+    print("no")
